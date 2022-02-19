f6d3a696d7b1642e4d0c69faa1f978fbd0bae85a
Kanishka <kanishkawickramasinghe50@gmail.com>
2022-02-19 15:54:58 +1100

    second commit

--- chatroom.py
+++ chatroom.py
@@ -1,1 +1,34 @@
-print("hello")
+"""
+Code inspired by FreeCodeCamp: youtube.com/watch?v=FGdiSJakIS4&ab_channel=freeCodeCamp.org
+"""
+
+import threading
+import socket
+
+host = '127.0.0.1'  # localhost
+
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+names = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.receive(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = names[index]
